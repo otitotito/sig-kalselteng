166e8d637af26eecaf28f8f5698925562d683d43
otitotito <81319766+otitotito@users.noreply.github.com>
2024-02-23 09:34:39 +0800

    Update layouts/header
    Update layouts/header dengan tidak memasukkan log akses ke database.

--- layouts/header.php
+++ layouts/header.php
@@ -1,26 +1,26 @@
 <?php
-session_start();
+// session_start();
 
-include_once('config/koneksi.php');
-require_once('config/base_url.php');
+// include_once('config/koneksi.php');
+// require_once('config/base_url.php');
 
-$waktu_akses = date('Y-m-d H:i:s');
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != '') {
-    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip_address = $_SERVER['REMOTE_ADDR'];
-}
+// $waktu_akses = date('Y-m-d H:i:s');
+// if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != '') {
+//     $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+// } else {
+//     $ip_address = $_SERVER['REMOTE_ADDR'];
+// }
 
-$_SESSION['last_activity'] = $waktu_akses;
+// $_SESSION['last_activity'] = $waktu_akses;
 
-if ($ip_address != '10.29.254.234') {
-    $query_log = mysqli_prepare($config_mysql, "INSERT INTO log_akses (waktu_akses, ip_address) VALUES (?, ?)");
-    mysqli_stmt_bind_param($query_log, "ss", $waktu_akses, $ip_address);
-    mysqli_stmt_execute($query_log);
-    mysqli_stmt_close($query_log);
-}
+// if ($ip_address != '10.29.254.234') {
+//     $query_log = mysqli_prepare($config_mysql, "INSERT INTO log_akses (waktu_akses, ip_address) VALUES (?, ?)");
+//     mysqli_stmt_bind_param($query_log, "ss", $waktu_akses, $ip_address);
+//     mysqli_stmt_execute($query_log);
+//     mysqli_stmt_close($query_log);
+// }
 
-mysqli_close($config_mysql);
+// mysqli_close($config_mysql);
 
 ?>
 
